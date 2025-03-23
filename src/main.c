edc117fae6b3cb8793cea3676c526632ba242257
Duan-Espinoza <75913012+Duan-Espinoza@users.noreply.github.com>
2025-03-22 19:16:41 -0600

    arreglos

--- src/main.c
+++ src/main.c
@@ -1,41 +1,44 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "models/database.h"
 #include "models/admin.h"
 #include <stdio.h>
 
+
 void menu_principal() {
-    // ... código existente ...
+    printf("\n=== SISTEMA DE PUNTO DE VENTA ===\n");
+    printf("1. Acceso administrativo\n");
+    printf("2. Opciones Generales\n");
+    printf("3. Salir\n");
+    printf("Seleccione: ");
 }
 
 int main() {
     MYSQL* conn = conectar_db();
     int opcion;
-    
+
     do {
         menu_principal();
         scanf("%d", &opcion);
-        
-        switch (opcion) {
+
+        switch(opcion) {
             case 1:
-                if (autenticar_admin(conn)) {
+                if(autenticar_admin(conn)) {
                     menu_administrativo(conn);
                 } else {
                     printf("\n¡Credenciales incorrectas!\n");
                 }
                 break;
             case 2:
-                // Consultar catálogo (implementar después)
+                printf("\n-- Módulo no implementado --\n");
                 break;
             case 3:
-                printf("Saliendo...\n");
+                printf("Saliendo del sistema...\n");
                 break;
             default:
-                printf("Opción inválida.\n");
+                printf("Opción no válida\n");
         }
-    } while (opcion != 3);
+    } while(opcion != 3);
 
     desconectar_db(conn);
     return 0;
