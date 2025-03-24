1cc1fbf1f2c8a52938a153b96cea817f018222ce
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-03-23 20:43:39 -0600

    Funcion terminada

--- src/main.c
+++ src/main.c
@@ -2,41 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void menuOpcionesGenerales();
-void mostrarMenuPrincipal();
-
-int main() {
-    int opcion;
-
-    while (1){
-        mostrarMenuPrincipal();
-        printf("Seleccione una opción: ");
-        scanf("%d", &opcion);
-        getchar();
-
-        switch (opcion) {
-            case 1:
-                //menuOpcionesAdministrativas();
-                break;
-            case 2:
-                menuOpcionesGenerales();
-                break;
-            case 3:
-                printf("Saliendo del sistema...\n");
-                return 0;
-            default:
-                printf("Opción inválida\n");
-    }
-}
-
-
 void mostrarMenuPrincipal() {
     printf("\n--- Sistema de Punto de Venta ---\n");
     printf("1. Opciones Administrativas\n");
     printf("2. Opciones Generales\n");
     printf("3. Salir\n");
 }
-
 
 void menuOpcionesGenerales() {
     int opcion;
@@ -70,4 +41,31 @@ void menuOpcionesGenerales() {
                 printf("Opción inválida\n");
         }
     }
-}}
+}
+
+
+int main() {
+    int opcion;
+
+    while (1){
+        mostrarMenuPrincipal();
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+        getchar();
+
+        switch (opcion) {
+            case 1:
+                //menuOpcionesAdministrativas();
+                break;
+            case 2:
+                menuOpcionesGenerales();
+                break;
+            case 3:
+                printf("Saliendo del sistema...\n");
+                return 0;
+            default:
+                printf("Opción inválida\n");
+    }
+}
+    return 0;
+}
